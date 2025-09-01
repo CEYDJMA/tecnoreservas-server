d8be068a9401c37082931a87c4b265cd4358c3be
Adferatuz <viadfeur@gmail.com>
2025-09-01 09:52:52 -0500

    feat(resource-service): Define la estructura de la capa de servicio para Recursos
    Introduce la interfaz IResourceService en un nuevo directorio service/interfaces para definir el contrato del servicio.
    
    La clase ResourceService ahora implementa esta interfaz, estableciendo una clara separación entre la definición y la implementación.
    
    Se inyecta ResourceRepository mediante el constructor y se añaden los stubs de los métodos requeridos por la interfaz.
    
    Este cambio sigue las mejores prácticas de Spring Boot, mejorando el desacoplamiento y la capacidad de prueba del código.

--- src/main/java/com/farukgenc/boilerplate/springboot/service/ResourceService.java
+++ src/main/java/com/farukgenc/boilerplate/springboot/service/ResourceService.java
@@ -1,7 +1,45 @@
 package com.farukgenc.boilerplate.springboot.service;
 
+import com.farukgenc.boilerplate.springboot.model.Resource;
+import com.farukgenc.boilerplate.springboot.repository.ResourceRepository;
+import com.farukgenc.boilerplate.springboot.service.interfaces.IResourceService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public class ResourceService {
+public class ResourceService implements IResourceService {
+
+    private final ResourceRepository resourceRepository;
+
+    public ResourceService(ResourceRepository resourceRepository) {
+        this.resourceRepository = resourceRepository;
+    }
+
+    @Override
+    public Resource create(Resource resource) {
+        return null;
+    }
+
+    @Override
+    public Optional<Resource> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Page<Resource> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public Resource update(Long id, Resource resourceDetails) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
 }
