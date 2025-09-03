7d0775f77f9401d4393e98f403387ebcef30b8ac
Adferatuz <viadfeur@gmail.com>
2025-09-03 09:57:41 -0500

    feat: Agregar campos de auditoría y configurar entidad Resource para persistencia
    - Agregar @CreationTimestamp y @UpdateTimestamp a Resource
    - Configurar columnas created_date y updated_date con restricciones apropiadas
    - Agregar configuraciones de actualización automática para fechas de auditoría
    - Establecer columna created_date como no actualizable (updatable = false)
    - Preparar entidad Resource para integración completa con JPA y base de datos
    
    Cambios técnicos:
    - @CreationTimestamp en campo createdDate (no actualizable)
    - @UpdateTimestamp en campo updatedDate (actualizable)
    - Nombres de columna explícitos para compatibilidad con BD
    - Restricciones de nulidad apropiadas para campos de auditoría

--- src/main/java/com/farukgenc/boilerplate/springboot/model/Resource.java
+++ src/main/java/com/farukgenc/boilerplate/springboot/model/Resource.java
@@ -7,7 +7,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -42,6 +45,14 @@ public abstract class Resource {
 
     @Column(nullable = false)
     private String brand;
+
+    @CreationTimestamp
+    @Column(name = "created_date", nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    @UpdateTimestamp
+    @Column(name = "updated_date", nullable = false)
+    private LocalDateTime updatedDate;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "service_line_id", nullable = false)
