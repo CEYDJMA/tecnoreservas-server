2c903874e8c6dba3e5101bdc209ef203acb8caa7
fmaovs <freddymauriciovillanuevasilva@gmail.com>
2025-09-17 12:11:59 -0500

    create service to save log session register

--- src/main/java/com/farukgenc/boilerplate/springboot/service/SessionLogService.java
+++ src/main/java/com/farukgenc/boilerplate/springboot/service/SessionLogService.java
@@ -1,7 +1,35 @@
 package com.farukgenc.boilerplate.springboot.service;
 
+import com.farukgenc.boilerplate.springboot.model.SessionLog;
+import com.farukgenc.boilerplate.springboot.model.User;
+import com.farukgenc.boilerplate.springboot.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class SessionLogService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public String registerLog(HttpServletRequest request){
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        final UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        User user = userRepository.findByUsername(userDetails.getUsername());
+        SessionLog sessionLog = new SessionLog();
+        sessionLog.setUser(user);
+
+        sessionLog.setLoginAt(LocalDateTime.now());
+
+        sessionLog.setIpAddress(request.getRemoteAddr());
+
+        return user.getUsername() + " se ha conectado el " + sessionLog.getLoginAt() + " desde " + request.getRemoteAddr();
+    }
+
 }
