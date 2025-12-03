e7fe85f3ab32e4a3335518bed8f48b880c13da05
fmaovs <freddymauriciovillanuevasilva@gmail.com>
2025-12-03 10:51:03 -0500

    reservation with resources for expert

--- src/main/java/com/farukgenc/boilerplate/springboot/service/TalentService.java
+++ src/main/java/com/farukgenc/boilerplate/springboot/service/TalentService.java
@@ -193,8 +193,8 @@ public class TalentService {
         return talents;
     }
 
-    public ReservationResponse createReservationWithResources(CreateReservationWithResourcesRequest reservationWithResourcesRequest){
-        if (reservationWithResourcesRequest.getResourceIds().isEmpty()){
+    public ReservationResponse createReservationWithResources(ReservationWithResourcesRequest reservationWithResourcesRequest){
+        if (reservationWithResourcesRequest.getResourceIds().isEmpty() || reservationWithResourcesRequest.getResourceIds().get(0) == 0){
             reservationWithResourcesRequest.setResourceIds(null);
         }
         return reservationService.createReservationWithResource(reservationWithResourcesRequest);
